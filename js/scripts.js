cfcc4b0ec82eb3a3bac44e1751007bcd7539a361
amymary89 <amygelbar@gmail.com>
2023-03-06 16:06:23 -0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+let pokemonList= [
+    {name: 'Charmander', type: 'fire' , height: 2},
+    {name: 'Squirtle', type: 'water', height: 1},
+    {name: 'Pikachu', type: 'electric' , height: 1.4},
+    {name: 'Jigglypuff', type: ['normal','fairy'], height: 1.8}]
+
+
+for(let i=0; i < pokemonList.length; i++){
+    if(pokemonList[i].height > 1.9){
+        document.write( pokemonList[i].name + ',' + ' ' + '(height:' + ' ' + pokemonList[i].height + ')' + '-' + 'Wow! That is big!')
+    }else{
+        document.write('<p>' + pokemonList[i].name + ',' + ' ' + '(height:' + pokemonList[i].height + ')' + '</p>')
+    }
+}
