c8d3e67ecb11eb933ccd0a77e2f2c76ec59b4ec2
amymary89 <amygelbar@gmail.com>
2023-09-06 08:09:58 -0400

    Update scripts.js
    Added the IIFE to the pokemon array with forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-let pokemonList= [
-    {name: 'Charmander', type: 'fire' , height: 2},
-    {name: 'Squirtle', type: 'water', height: 1},
-    {name: 'Pikachu', type: 'electric' , height: 1.5},
-    {name: 'Jigglypuff', type: ['normal','fairy'], height: 1.8}]
+//let pokemonList= [
+//    {name: 'Charmander', type: 'fire' , height: 2},
+ //   {name: 'Squirtle', type: 'water', height: 1},
+//    {name: 'Pikachu', type: 'electric' , height: 1.5},
+//    {name: 'Jigglypuff', type: ['normal','fairy'], height: 1.8}]
 
 
 //for(let i=0; i < pokemonList.length; i++){
@@ -12,7 +12,30 @@ let pokemonList= [
   //      document.write('<p>' + pokemonList[i].name + ',' + ' ' + '(height:' + pokemonList[i].height + ')' + '</p>')
   //  }
 //}
+
+
+let pokemonRepository = (function () {
+    let pokemonList= [
+        {name: 'Charmander', type: 'fire' , height: 2},
+        {name: 'Squirtle', type: 'water', height: 1},
+        {name: 'Pikachu', type: 'electric' , height: 1.5},
+        {name: 'Jigglypuff', type: ['normal','fairy'], height: 1.8}]
+    
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+    
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
 function myLoopFunction(pokemon) {
     console.log(pokemon.name + ' is ' + pokemon.type + ' type ' + pokemon.height + ' height. ');
 }
-pokemonList.forEach(myLoopFunction);
+pokemonRepository.getAll().forEach(myLoopFunction)
